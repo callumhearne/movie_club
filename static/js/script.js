d045b19bd60e404a4c3e4b0a5384c0057ca63dcb
callumhearne <callum.hearne98@gmail.com>
2021-02-09 12:38:55 +0000

    added review boxes

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,8 @@
   $(document).ready(function(){
     $('.sidenav').sidenav({edge: "right"}, {draggable: true});
   });
+
+    $(document).ready(function(){
+    $('.collapsible').collapsible();
+  });
+        
